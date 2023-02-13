3dc31ae0b24e30e69984084e2efb25178a211a5f
Isaac Ramirez <iramirezc@live.com.mx>
2023-02-14 00:57:40 +0100

    Create basic hello world React App.

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const App = () => {
+  console.log("Hello World!");
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <span>from Debris app</span>
+    </>
+  );
+};
