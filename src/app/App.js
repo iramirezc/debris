2737e2162ce4b85161964b68302ccfb42a35c453
Isaac Ramirez <iramirezc@live.com.mx>
2023-02-15 23:53:53 +0100

    Create HelloWorld component.

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,9 @@
 import React from "react";
+import { HelloWorld } from "../components/HelloWorld";
 
-export const App = () => {
-  console.log("Hello World!");
-
-  return (
-    <>
-      <h1>Hello World!</h1>
-      <span>from Debris app</span>
-    </>
-  );
-};
+export const App = () => (
+  <>
+    <HelloWorld />
+    <span>from Debris app</span>
+  </>
+);
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const HelloWorld = () => {
+  console.log("Hello World!");
+
+  return <h1>Hello World!</h1>;
+};
