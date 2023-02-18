7b16b90d8d160ea766a73e3a474dc3a1c8d1db3d
Isaac Ramirez <iramirezc@live.com.mx>
2023-02-18 13:48:42 +0100

    Connect App with Redux provider.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import React from "react"
+import { Provider } from "react-redux"
+import store from "app/store"
 import { HelloWorld } from "components"
 
 export const App = () => (
-  <>
+  <Provider store={store}>
     <HelloWorld />
     <span>from Debris app</span>
-  </>
+  </Provider>
 )
