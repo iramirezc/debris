a409e16e48c79bc8f54cb0fdf1e13e989745ea31
Isaac Ramirez <iramirezc@live.com.mx>
2023-02-19 17:36:30 +0100

    Update config for Storybook.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,35 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true
   },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:storybook/recommended",
     "prettier"
   ],
-  overrides: [],
+  overrides: [
+    {
+      files: ["*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+      rules: {
+        "@typescript-eslint/no-unsafe-assignment": 0,
+        "@typescript-eslint/no-unsafe-member-access": 0,
+        "@typescript-eslint/no-unsafe-argument": 0,
+        "@typescript-eslint/await-thenable": 0
+      }
+    }
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     project: true,
     sourceType: "module"
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint", "storybook"],
   root: true,
   rules: {},
   settings: {
